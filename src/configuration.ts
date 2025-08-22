dbc9f1ef224c8b75a37e4267aa02d82735a6f32e
Jon Tiritilli <jon.tiritilli@domo.com>
2025-08-22 14:03:50 -0700

    update to use embed endpoint, add mock data

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,8 +1,4 @@
-export const cardUrls = [
-  'https://domo-es.domo.com/embed/card/private/vQBA0',
-  'https://domo-es.domo.com/embed/card/private/w0DB1',
-  'https://domo-es.domo.com/embed/card/private/16lRV',
-];
+export const cardUrls = ['http://localhost:3001/embed/items/1'];
 
 export const dropdownColumn = 'clientType';
 export const dropdownLabel = 'Client Type';
